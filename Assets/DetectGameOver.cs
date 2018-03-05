745424737a9268db1b8a3e8c5a17a7d7f6063ddd
Ayo/Fouad Animashaun <x16300351@student.ncirl.ie>
2018-03-05 11:10:07 +0000

    Additional functionality

--- Assets/DetectGameOver.cs
+++ Assets/DetectGameOver.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof (BoxCollider2D))]
 public class DetectGameOver : MonoBehaviour {
 
+	Text txt;
+	private string updateText;
+
 	void Start(){
+		txt = gameObject.GetComponents<Text> ();
+		txt.text = "Reach the top!";
 		if (gameObject.GetComponent<BoxCollider2D> () == null) {
 		}
 	}
+
+
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag.Equals("Player")){
 			Time.timeScale = 0;
-			Debug.Log ("Game Over");
+			updateText = PlayerPrefs.GetString ("text");
+			PlayerPrefs.SetString("Game Over!");
+			Debug.Log ("Game Over"); 
 		}
 	}
 }
---
+++ Assets/TextMovement.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var textSpeed = 2.0;
+var timer = 0.0;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	timer += Time.deltaTime;
+	transform.Translate(Vector3(0, textSpeed * Time.deltaTime, 0));
+	transform.localScale += Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+	if(timer > 2.0)
+	{
+	Destroy(gameObject);
+	}
+}
