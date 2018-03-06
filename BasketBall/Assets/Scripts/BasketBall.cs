0b7908aa940556d44053ee62ace1f1c2ef50f4bd
Ayo/Fouad Animashaun <x16300351@student.ncirl.ie>
2018-03-06 09:48:07 +0000

    BasketBall Game
    FreeThrow basketball game

---
+++ BasketBall/Assets/Scripts/BasketBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasketBall : MonoBehaviour {
+
+	public Text score;
+
+	private int currentScore = 0;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Ring") {
+			ScoreUpdate();
+		} 
+	}
+
+	private void ScoreUpdate()
+	{
+		currentScore++;
+		score.text = currentScore.ToString();
+	}
+}
